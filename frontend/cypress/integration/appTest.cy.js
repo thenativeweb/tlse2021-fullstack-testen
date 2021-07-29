d0727946d3049ddf0a38f464db844c56b9146f50
David Losert <david.losert@gmx.de>
2021-07-29 15:15:30 +0200

    chore: Moves e2e tests and adds contributors

--- frontend/cypress/integration/appTest.cy.js
+++
@@ -1,9 +0,0 @@
-/* eslint-disable mocha/no-synchronous-tests */
-
-describe('Application Tests', () => {
-  it('loads the page.', () => {
-    cy.visit('/');
-
-    cy.contains('Ticketeer');
-  });
-});
